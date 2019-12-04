10ef9339f057b139eaebaa709a06182a64df7e08
diegodu <34308601+diegodu@users.noreply.github.com>
2019-12-04 12:52:54 -0500

    base de datos

--- config/conexionBD.php
+++ config/conexionBD.php
@@ -1,9 +1,9 @@
 <?php
 
- $db_servername = "remotemysql.com";
- $db_username = "8rF3vDKJrF";
- $db_password = "RC3zx2wKZN";
- $db_name = "8rF3vDKJrF";
+ $db_servername = "us-cdbr-iron-east-05.cleardb.net";
+ $db_username = "b93979de55c556";
+ $db_password = "3efa1ec0";
+ $db_name = "heroku_45fa3d31db2575f";
 
  $conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
  $conn->set_charset("utf8");
