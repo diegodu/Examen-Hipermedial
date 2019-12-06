00704484c0db874ff5a02e89e41674b4b61a002f
pabloAMD <pablo.malla.abc.com@gmail.com>
2019-12-06 00:28:24 -0500

    cambios pagina usuario logeado

--- Public/Vista/include/header.php
+++ Public/Vista/include/header.php
@@ -1,47 +1,82 @@
 <header>
     <div class="bar-login">
         <div class="btns container">
-           
-            <?php 
-                if (isset($_SESSION['isLogged'])) {
-                  ?>
-                     <a href="../Vista/carrito.php"><i class="fas fa-cart-arrow-down"></i></a>
-                     <a href="../Vista/crear_usuario.php"><i class="fas fa-user"></i> <?php echo ($_SESSION['usu_nombre']." ".$_SESSION['usu_apellido'])?></a>
-                     <a href="../../config/Cerrar_sesion.php"><i class="fas fa-user"></i> Cerrar Sesion</a>
-                  <?php
-                }else{
 
-                    ?> 
-                     <a href="../Vista/login.php"><i class="fas fa-sign-in-alt"></i> Iniciar sesion</a>
-                    <a href="../Vista/crear_usuario.php"><i class="fas fa-user"></i> Registro</a>
-                    <?php 
-                }
-            
+            <?php
+            if (isset($_SESSION['isLogged'])) {
+                ?>
+                <a href="../Vista/carrito.php"><i class="fas fa-cart-arrow-down"></i></a>
+                <a href="../Vista/modifica_usuario.php"><i class="fas fa-user"></i> <?php echo ($_SESSION['usu_nombre'] . " " . $_SESSION['usu_apellido']) ?></a>
+                <a href="../../config/Cerrar_sesion.php"><i class="fas fa-user"></i> Cerrar Sesion</a>
+            <?php
+            } else {
+
+                ?>
+                <a href="../Vista/login.php"><i class="fas fa-sign-in-alt"></i> Iniciar sesion</a>
+                <a href="../Vista/crear_usuario.php"><i class="fas fa-user"></i> Registro</a>
+            <?php
+            }
+
             ?>
-           
+
         </div>
     </div>
     <h1>LIBRERIA</h1>
-    <nav class="nav-bar container">
-        <ul>
-            <li><a href="../Vista/index.php">Inicio</a></li>
-            <li><a href="../Vista/cat_seleccionada.php">Categorias</a>
-            </li>
-            <li><a href="./quiens.php">Quienes somos</a>
-            </li>
-            <li><a href="#contactos">Contactos</a></li>
-            <li>
-                <div class="search-content">
-                    <input onkeyup="buscar(this, 'index')" type="search" name="" id="" placeholder="Buscar">
-                    <i class="fas fa-search"></i>
-                    <div class="listaBuscarLibro">
-                    
-                    </div>
-                   
 
-                </div>
-            </li>
-        </ul>
-    </nav>
+    <?php
+    if (isset($_SESSION['isLogged'])) {
+        ?>
+        <nav class="nav-bar container">
+            <ul>
+                <li><a href="../Vista/index.php">Inicio</a></li>
+                <li><a href="../Vista/cat_seleccionada.php">Categorias</a>
+                </li>
+                <li><a href="./quiens.php">Quienes somos</a>
+                </li>
+                <li><a href="#contactos">Contactos</a></li>
+                <li><a href="#">Pedidos</a></li>
+                <li>
+                    <div class="search-content">
+                        <input onkeyup="buscar(this, 'index')" type="search" name="" id="" placeholder="Buscar">
+                        <i class="fas fa-search"></i>
+                        <div class="listaBuscarLibro">
+
+                        </div>
+
+
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    <?php
+    } else {
+
+        ?>
+        <nav class="nav-bar container">
+            <ul>
+                <li><a href="../Vista/index.php">Inicio</a></li>
+                <li><a href="../Vista/cat_seleccionada.php">Categorias</a>
+                </li>
+                <li><a href="./quiens.php">Quienes somos</a>
+                </li>
+                <li><a href="#contactos">Contactos</a></li>
+                <li>
+                    <div class="search-content">
+                        <input onkeyup="buscar(this, 'index')" type="search" name="" id="" placeholder="Buscar">
+                        <i class="fas fa-search"></i>
+                        <div class="listaBuscarLibro">
+
+                        </div>
+
+
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    <?php
+    }
+
+    ?>
+
 
 </header>
