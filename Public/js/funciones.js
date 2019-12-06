a77c6b8ef15023d00e2c855b5f8edee8fcdaaee2
pabloAMD <pmalla@ups.edu.ec>
2019-12-05 22:11:34 -0500

    cambios en el carrito

--- Public/js/funciones.js
+++ Public/js/funciones.js
@@ -30,6 +30,23 @@ function like() {
     xmlhttp.send()
 }
 
+function agregarCarrito(codigoL){
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("valorlike").innerHTML = this.responseText
+            document.getElementById("NotificaCarrito").innerHTML = this.responseText
+        }
+    };
+    xmlhttp.open("GET", "../Controlador/categoria.php?categoria=" + element.textContent, true)
+    xmlhttp.open("GET", "../Controlador/agregar_carrito.php?codigoproducto=" + codigoL, true)
+    xmlhttp.send()
+}
+
 function actualizarCarrito(codCarrito, element) {
 
     if (window.XMLHttpRequest) {
@@ -60,39 +77,6 @@ function realizarPedido(){
     xmlhttp.open("GET", "../Controlador/agregarPedido.php", true)
     xmlhttp.send()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function darLike(corazon){ 
